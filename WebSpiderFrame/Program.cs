75e19a6e801a21ac8dbc58111758b4930503e81f
wolfben <363273099@qq.com>
2017-03-24 16:31:58 +0800

    commit

---
+++ WebSpiderFrame/Program.cs
@@ -0,0 +1,56 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Utility.Helper;
+
+namespace WebSpiderFrame
+{
+    class Program
+    {
+        /// <summary>
+        /// Demo示例，抓取天眼查网站的数据（wwww.tianyancha.com），这个网站反爬虫比较厉害，一般的方法获取重要数据基本没用
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            WebDriverHelper driver = new WebDriverHelper();
+
+            var html = driver.GetPhantomJSSpider("http://bj.tianyancha.com/search");//通过PhantomJs来渲染列表页
+
+            if (!string.IsNullOrEmpty(html))
+            {
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(html);
+
+                HtmlNode node = doc.DocumentNode;
+
+                var nodeList = node.SelectNodes("//div[@class='search_right_item']//a[contains(@class,'query_name')]");
+
+                if (nodeList == null)
+                {
+                    return;
+                }
+
+                foreach (var item in nodeList)
+                {
+                    var link = item.Attributes["href"].Value;
+                    Console.WriteLine("{0}的url：{1}", item.InnerText, link);
+
+                    var html2 = driver.GetChromeSpider(link);//通过Chrome游览器来渲染详细页（详细页用PhantomJs有些元素渲染不出来）
+                    driver.Driver.Quit();
+                    if(!string.IsNullOrEmpty(html2))
+                    {
+
+                    }
+                }
+            }
+
+            
+            driver.Driver.Dispose();
+        }
+    }
+}
---
+++ WebSpiderFrame/WebDriverHelper.cs
@@ -0,0 +1,52 @@
+﻿using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Utility.Helper;
+using OpenQA.Selenium.Chrome;
+
+
+namespace Utility.Helper
+{
+    /// <summary>
+    /// WebDriver帮助类，详情见http://seleniumhq.github.io/selenium/docs/api/dotnet/
+    /// 其中在程序exe目录下需要phantomjs.exe，chromedriver.exe等各种驱动程序
+    /// </summary>
+    public class WebDriverHelper
+    {
+        public IWebDriver Driver { get; set; }
+
+        public  string GetChromeSpider(string url)
+        {
+            Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl(url);
+            return Driver.PageSource;
+        }
+
+        public  string GetPhantomJSSpider(string url)
+        {
+            Driver = new PhantomJSDriver();
+            Driver.Navigate().GoToUrl(url);
+            return Driver.PageSource;
+        }
+
+        /// <summary>
+        /// 设置代理服务器
+        /// </summary>
+        /// <returns></returns>
+        private  PhantomJSDriverService GetPhantomJSDriverService()
+        {
+            PhantomJSDriverService pds = PhantomJSDriverService.CreateDefaultService();
+            //设置代理服务器地址
+            //pds.Proxy = $"{ip}:{port}"; 
+            //设置代理服务器认证信息
+            //pds.ProxyAuthentication = GetProxyAuthorization();
+            return pds;
+        }
+    }
+}
